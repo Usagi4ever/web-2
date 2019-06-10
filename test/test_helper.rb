53364ebfebe28b47eeb01473374d5843fefae0dd
EC2 Default User <ec2-user@ip-172-31-28-187.us-east-2.compute.internal>
2019-06-10 14:48:01 +0000

    Finish user edit, update, index, and destroy actions

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,20 +1,18 @@
 ENV['RAILS_ENV'] ||= 'test'
-require 'test_helper'
-
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
-
 class ActiveSupport::TestCase
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-include ApplicationHelper
+  include ApplicationHelper
   # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
 
-  # Log in as a particular user.
+  # log in as a particular user.
   def log_in_as(user)
     session[:user_id] = user.id
   end
